1c87f6b20369672cae8cb93e8bb1762671a15901
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-07 21:11:55 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+-- https://discord.gg/cfxdev for support
+-- https://discord.gg/cfxdev for support
+-- https://discord.gg/cfxdev for support
+
+Config.robberyTime = 200
+Config.rewardAmount = 1000
+Config.maxDistance = 30.0
+Config.robberyCooldown = 200
+Config.Webhook = "URL_HERE"
+Config.stores = {
+    {name = "24/7", x = 1959.0280, y = 3748.5815, z = 32.3437},
+    {name = "24/7", x = -92.8165, y = 656.2870, z = 14.8092},
+    {name = "24/7", x = 1121.5872, y = -50.4533, z = 14.3588},
+    {name = "24/7", x = 759.7999, y = 2232.6267, z = 32.7023},
+}
