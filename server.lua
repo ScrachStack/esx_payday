df00fb71d1b61aecd1e5673a70d1b406b03e6872
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-07 19:22:00 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -80,5 +80,51 @@ AddEventHandler('zaps:storeRobberyStarted', function(storeName)
             
         end
     end
+
+        function zapsupdatee()
+    local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
+    local resourceName = "esxpayday" 
+    
+    PerformHttpRequest(githubRawUrl, function(statusCode, responseText, headers)
+        if statusCode == 200 then
+            local responseData = json.decode(responseText)
+    
+            if responseData[resourceName] then
+                local remoteVersion = responseData[resourceName].version
+                local description = responseData[resourceName].description
+                local changelog = responseData[resourceName].changelog
+    
+                local manifestVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    
+                print("Resource: " .. resourceName)
+                print("Manifest Version: " .. manifestVersion)
+                print("Remote Version: " .. remoteVersion)
+                print("Description: " .. description)
+    
+                if manifestVersion ~= remoteVersion then
+                    print("Status: Out of Date (New Version: " .. remoteVersion .. ")")
+                    print("Changelog:")
+                    for _, change in ipairs(changelog) do
+                        print("- " .. change)
+                    end
+                    print("Link to Updates:  https://discord.gg/cfxdev")
+                else
+                    print("Status: Up to Date")
+                end
+            else
+                print("Resource name not found in the response.")
+            end
+        else
+            print("HTTP request failed with status code: " .. statusCode)
+        end
+    end, "GET", nil, json.encode({}), {})
+    end
+    AddEventHandler('onResourceStart', function(resource)
+        if resource == 'esx_payday' then
+            zapsupdatee()
+        else 
+            print("[ALERT!!! Please rename your resource to esx_payday") -- Please do not edit this is how I keep track of how many servers use it.
+        end
+    end)
 end)
 
